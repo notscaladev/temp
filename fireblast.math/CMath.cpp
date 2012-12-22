ed395f486267e50ae049168dc6e80a68db45802e
Viacheslav Okolitiy <justforgit@gmail.com>
2012-12-22 21:26:40 +0200

    Some changes

--- fireblast.math/CMath.cpp
+++ fireblast.math/CMath.cpp
@@ -1,8 +1,12 @@
 #include "stdafx.h"
 #include "IMath.h"
+#include <random>
+#include <math.h>
+
 using namespace IMath;
 
-int IMath::signum(int n){
+int IMath::signum(int n)
+{
 	if(n == 0)
 	{
 	   return 0;
@@ -39,4 +43,134 @@ void IMath::arraySumInternal(int val[])
 	{
 		val[i] = val[i-1] + val[i];
 	}
+}
+
+float IMath::cross(const float pXA, const float pYA, const float pZA, const float pXB, const float pYB, const float pZB)
+{
+	return pXA * pYA * pZA - pXB * pYB * pZB;
+}
+
+float IMath::dot(const float pXA, const float pYA, const float pZA, const float pXB, const float pYB, const float pZB)
+{
+	return pXA * pXB + pYA * pYB + pZA * pZB;
+}
+
+bool IMath::isOdd(int number)
+{
+	return number % 2 == 1;
+}
+
+bool IMath::isEven(int number)
+{
+	return number % 2 == 0;
+}
+
+float IMath::mix(float pX, float pY, float pZ, float pMix)
+{
+	return pX * (1 - pMix) + pY * pMix;
+}
+
+float IMath::length(const float pX, const float pY, const float pZ)
+{
+	return sqrt((pX * pX) + (pY * pY) + (pZ * pZ));
+}
+
+float IMath::distance(const float pX1, const float pY1, const float pZ1, const float pX2, const float pY2, const float pZ2)
+{
+	const float dX = pX2 - pX1;
+	const float dY = pY2 - pY1;
+	const float dZ = pZ2 - pZ1;
+	return sqrt((dX * dX) + (dY * dY) + (dZ * dZ));
+}
+
+float IMath::bringToBounds(const float pMinValue, const float pMaxValue, const float pValue)
+{
+	return max(pMinValue, min(pMaxValue, pValue));
+}
+
+int IMath::bringToBounds(const int pMinValue, const int pMaxValue, const int pValue)
+{
+	return max(pMinValue, min(pMaxValue, pValue));
+}
+
+bool IMath::isInBounds(const float pMinValue, const float pMaxValue, const float pValue)
+{
+	return pValue >= pMinValue && pValue <= pMaxValue;
+}
+
+bool IMath::isInBounds(const int pMinValue, const int pMaxValue, const int pValue)
+{
+	return pValue >= pMinValue && pValue <= pMaxValue;
+}
+
+float IMath::arraySum(const float pValues[])
+{
+	float sum = 0;
+	const int valueCount = sizeof(pValues);
+
+	for(int i = 0; i < valueCount; i++)
+	{
+		sum += pValues[i];
+	}
+
+	return sum;
+}
+
+void IMath::arraySumInto(long pValues[], long pTargetValues[], long pFactor)
+{
+	pTargetValues[0] = pValues[0] * pFactor;
+	const int valueCount = sizeof(pValues);
+
+	for(int i = 1; i < valueCount; i++)
+	{
+		pTargetValues[i] = pTargetValues[i-1] + pValues[i] * pFactor;
+	}
+}
+
+void IMath::arraySumInternal(long pValues[], long pFactor)
+{
+	pValues[0] = pValues[0] * pFactor;
+	const int valueCount = sizeof(pValues);
+
+	for(int i = 1; i < valueCount; i++)
+	{
+		pValues[i] = pValues[i-1] + pValues[i] * pFactor;
+	}
+}
+
+void IMath::arraySumInternal(long pValues[])
+{
+	int valueCount = sizeof(pValues);
+	for(int i = 1; i < valueCount; i++)
+	{
+		pValues[i] = pValues[i-1] + pValues[i];
+	}
+}
+
+int IMath::nextPowerOfTwo(int n)
+{
+	int k = n;
+
+	if (k == 0) 
+	{
+		return 1;
+	}
+
+	k--;
+
+	for (int i = 1; i < 32; i <<= 1) {
+		k = k | k >> i;
+	}
+
+	return k + 1;
+}
+
+int IMath::nextPowerOfTwo(float f)
+{
+	return nextPowerOfTwo((int)(ceil(f)));
+}
+
+bool IMath::isPowerOfTwo(int n)
+{
+	return ((n != 0) && (n & (n - 1)) == 0);
 }
--- fireblast.math/IMath.h
+++ fireblast.math/IMath.h
@@ -8,6 +8,42 @@ namespace IMath
 	int sum(int values[]);
 
 	void arraySumInternal(int pValues[]);
+
+	float cross(const float pXA, const float pYA, const float pZA, const float pXB, const float pYB, const float pZB);
+
+	float dot(const float pXA, const float pYA, const float pZA, const float pXB, const float pYB, const float pZB);
+
+	bool isOdd(int num);
+
+	bool isEven(int num);
+
+	float mix(float pX, float pY, float pZ, float pMix);
+
+	float length(const float pX, const float pY, const float pZ);
+
+	float distance(const float pX1, const float pY1, const float pZ1, const float pX2, const float pY2, const float pZ2);
+
+	float bringToBounds(const float pMinValue, const float pMaxValue, const float pValue);
+
+	int bringToBounds(const int pMinValue, const int pMaxValue, const int pValue);
+	
+	bool isInBounds(const float pMinValue, const float pMaxValue, const float pValue);
+
+	bool isInBounds(const int pMinValue, const int pMaxValue, const int pValue);
+
+	float arraySum(const float pValues[]);
+
+	void arraySumInto(long pValues[], long pTargetValues[], long pFactor);
+
+	void arraySumInternal(long pValues[], long pFactor);
+
+	void arraySumInternal(long pValues[]);
+
+	int nextPowerOfTwo(int n);
+
+	int nextPowerOfTwo(float f);
+
+	bool isPowerOfTwo(int n);
 }
 #endif // !_IMATH_H_
 
