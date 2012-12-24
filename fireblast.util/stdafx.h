7fd2e231ab4e5904edad1a90f92e96001439a951
Viacheslav Okolitiy <justforgit@gmail.com>
2012-12-24 23:39:23 +0200

    Some changes

--- fireblast.util/stdafx.h
+++ fireblast.util/stdafx.h
@@ -7,6 +7,7 @@
 
 #include "targetver.h"
 #include "color.h"
+#include "generic_pool.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
